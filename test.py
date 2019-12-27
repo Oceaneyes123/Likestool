3760968e5b464d4828d2bd8c7eca0d321c662e5f
dave <jezreldavesondia@gmail.com>
2019-12-27 18:46:54 +0800

    seconf commit

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-from selenium import webdriver
-import os
+from selenium.webdriver import ChromeOptions
+from splinter import Browser
+from bs4 import BeautifulSoup
 from time import sleep
+from random import randint
+from halo import Halo
 
 
-driver = webdriver.Chrome('C:/Users/Dave/Downloads/Compressed/Compressed/chromedriver_win32/chromedriver.exe')
-driver.get("http://legacy.likestool.com/campaign/my-campaigns")
-driver.execute_script("document.getElementById('loginform-username').value = 'hideemail2@hideemail.net'; document.getElementById('loginform-password').value = 'oceaneyes0928'; document.getElementById('login_form').submit(); ")
+
+browser.visit("http://legacy.likestool.com/")
+browser.fill('LoginForm[username]', 'hideemail2@hideemail.net')
+browser.fill('LoginForm[password]', 'oceaneyes0928')
+browser.find_by_xpath('//*[@id="login_form"]/div[4]/input').click()
 sleep(10)
-driver.get("http://legacy.likestool.com/campaign/add-site-form")
-driver.execute_script("document.getElementById('campaign-type').value = '17'; document.getElementById('campaign-title').value = 'Subzz'; document.getElementById('campaign-url').value = 'https://www.youtube.com/channel/UC0G4Xh0DTGeqZxAVX66WATw'; document.getElementById('add_edit').submit();")
+browser.visit("http://legacy.likestool.com/campaign/add-site-form")
+element = browser.find_by_id('campaign-type').first
+element.select('17')
+browser.fill('Campaign[title]', 'Subzz')
+browser.fill('Campaign[url]', 'https://www.youtube.com/channel/UC0G4Xh0DTGeqZxAVX66WATw')
+browser.find_by_xpath('//*[@id="add_edit"]/div[4]/input').click()
+
