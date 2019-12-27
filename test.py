f0caea8abb494ee600a11a41b1f0f27bc71456a9
Oceaneyes123 <42128868+Oceaneyes123@users.noreply.github.com>
2019-12-27 18:56:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,15 +7,15 @@ from halo import Halo
 
 
 with Browser() as browser:
-browser.visit("http://legacy.likestool.com/")
-browser.fill('LoginForm[username]', 'hideemail2@hideemail.net')
-browser.fill('LoginForm[password]', 'oceaneyes0928')
-browser.find_by_xpath('//*[@id="login_form"]/div[4]/input').click()
-sleep(10)
-browser.visit("http://legacy.likestool.com/campaign/add-site-form")
-element = browser.find_by_id('campaign-type').first
-element.select('17')
-browser.fill('Campaign[title]', 'Subzz')
-browser.fill('Campaign[url]', 'https://www.youtube.com/channel/UC0G4Xh0DTGeqZxAVX66WATw')
-browser.find_by_xpath('//*[@id="add_edit"]/div[4]/input').click()
+    browser.visit("http://legacy.likestool.com/")
+    browser.fill('LoginForm[username]', 'hideemail2@hideemail.net')
+    browser.fill('LoginForm[password]', 'oceaneyes0928')
+    browser.find_by_xpath('//*[@id="login_form"]/div[4]/input').click()
+    sleep(10)
+    browser.visit("http://legacy.likestool.com/campaign/add-site-form")
+    element = browser.find_by_id('campaign-type').first
+    element.select('17')
+    browser.fill('Campaign[title]', 'Subzz')
+    browser.fill('Campaign[url]', 'https://www.youtube.com/channel/UC0G4Xh0DTGeqZxAVX66WATw')
+    browser.find_by_xpath('//*[@id="add_edit"]/div[4]/input').click()
 
